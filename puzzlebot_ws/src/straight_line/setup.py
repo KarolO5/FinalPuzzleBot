ace3c240b9949a5c7a5841a3e696dbe47ff84256
kalico05 <A01424317@tec.mx>
2026-05-17 22:05:17 -0600

    Recuperacion

---
+++ puzzlebot_ws/src/straight_line/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+package_name = 'straight_line'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'odometry = straight_line.odometry:main',
+            'pd_controller = straight_line.pd_controller:main',
+        ],
+    },
+)
